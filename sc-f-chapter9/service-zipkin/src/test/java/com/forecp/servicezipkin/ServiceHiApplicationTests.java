c2ecf4b7b585687ff6c20f68358d190e6efddb97
FCN\860120014 <zhanghao@fujisoft-chinacom>
2021-07-12 17:27:58 +0800

    代码提交

---
+++ sc-f-chapter9/service-zipkin/src/test/java/com/forecp/servicezipkin/ServiceHiApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.forecp.servicezipkin;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceHiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
